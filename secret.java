93eb258a6c5e22654b27267dbf49340c9e9e4f79
tembalii <159453738+tembalii@users.noreply.github.com>
2025-05-26 18:11:59 +0100

    Create secret.java

---
+++ secret.java
@@ -0,0 +1,10 @@
+public class ApiClient {
+    public static void main(String[] args) {
+        System.out.println("Using API Key: " + apiKey);
+    }
+} 
+public class ApiClient {
+    public static void main(String[] args) {
+        System.out.println("Using API key: " + apiKey);
+    }
+}
